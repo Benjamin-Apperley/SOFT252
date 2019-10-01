38ca5508792873fe78d3d6f386d55f7416288496
(s) Benjamin Apperley <benjamin.apperley@students.plymouth.ac.uk>
2019-10-01 12:55:14 +0100

    Bank example for classes

---
+++ Week01/BankExample1/src/bankentities/BankAccount.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankentities;
+
+/**
+ *
+ * @author bapperley
+ */
+public class BankAccount 
+{
+    private double balance;
+    private double overdraft;
+    private String holder;
+    
+    public BankAccount(String holder)
+    {
+        this.holder = holder;
+        this.overdraft = 500;
+        this.balance = 100;
+    }
+    
+    
+}
